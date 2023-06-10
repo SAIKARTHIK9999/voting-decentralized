a4dc9d9ee91a1f85f7c61092c4ed069a2343a814
karthik <67945756+SAIKARTHIK9999@users.noreply.github.com>
2023-06-10 14:52:37 +0530

    Metmask connected to Website

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import {useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import { contractAbi, contractAddress } from './constants/constant';
+import Login from './components/Login';
+import './App.css';
 
-const App = () => {
-  return (
-    <div>frontend</div>
-  )
+function App(){
+
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount ] = useState(null);
+  const [ isConnected, setIsConnected ] = useState(false);
+
+  async function connectToMetamask(){
+    if (window.ethereum) {
+      try{
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        setProvider(provider);
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        console.log("Metamask connected: "+address);
+        setIsConnected(true);
+      }
+      catch(e){
+        console.log(e);
+      }
+    }
+    else{
+      console.error("MetaMask not detected in the browser");
+    }
+  }
+    return (
+      <div className="App">
+        <Login connectWallet = {connectToMetamask}/>
+      </div>
+    )
 }
+
 
 export default App
---
+++ client/src/components/connected.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Connected = (props) => {
+  return (
+    <div className="login-container">
+        <h1 className="welcome-message">Welcome to decentralized voting website</h1>
+        <button className="login-button" onClick={props.connectWallet}>Login Metamask</button>
+    </div>
+  )
+}
+
+export default Connected
