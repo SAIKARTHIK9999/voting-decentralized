c9f22935f423c4bb1c68dfe9ccfb77c4f9991d72
karthik <67945756+SAIKARTHIK9999@users.noreply.github.com>
2023-06-10 16:59:25 +0530

    blockchain done

--- client/src/components/connected.jsx
+++ client/src/components/connected.jsx
@@ -1,12 +1,45 @@
 import React from 'react'
 
 const Connected = (props) => {
-  return (
-    <div className="login-container">
-        <h1 className="welcome-message">Welcome to decentralized voting website</h1>
-        <button className="login-button" onClick={props.connectWallet}>Login Metamask</button>
-    </div>
-  )
+    return (
+        <div className="connected-container">
+            <h1 className="connected-header">Your metamask account is connected to our website</h1>
+            <p className='connected-account'>Metamask Account: {props.account}</p>
+            <p className='connected-account'>Remaining Time: {props.remainingTime}</p>
+
+            {props.showButton ? (
+                <p className='connected-account'>You have aldready voted</p>
+            ) : (
+                <div>
+                    <input type="number" placeholder='Enter Candidate Index' value={props.number} onChange={props.handleNumberChange}></input>
+                    <br />
+                    <button className='login-button' onClick={props.voteFunction}>Vote</button>
+                </div>
+
+            )}
+
+
+            <table id="myTable" className="candidates-table">
+                <thead>
+                    <tr>
+                        <th>Index</th>
+                        <th>Candidate name</th>
+                        <th>Candidate votes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.candidates.map((candidate, index) => (
+                        <tr key={index}>
+                            <td>{candidate.index}</td>
+                            <td>{candidate.name}</td>
+                            <td>{candidate.voteCount}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+        </div>
+    )
 }
 
-export default Connected
+export default Connected;
